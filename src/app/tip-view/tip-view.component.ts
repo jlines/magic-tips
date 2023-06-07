4622ea1dacbf7b5d8e6c42a47b7fb53bb4639540
Jason Lines <jlines@solidstatenetworks.com>
2023-06-06 20:18:18 -0700

    add google analytics and other things

--- src/app/tip-view/tip-view.component.ts
+++ src/app/tip-view/tip-view.component.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http'
 import { Component, OnInit } from '@angular/core';
-import _ from 'lodash';
+import _, { List } from 'lodash';
 
 @Component({
   selector: 'app-tip-view',
@@ -9,15 +9,45 @@ import _ from 'lodash';
 })
 export class TipViewComponent implements OnInit {
   public comboData: any;
+  public testList: Array<string> = ['a','b','c','d'];
   public skillList: any;
   public constructor(private http: HttpClient) {}
   public ngOnInit(): void {
     const url: string = '/assets/combos.json';
     this.http.get(url).subscribe((response: any) => {
       this.comboData = response;
-      this.skillList = _.map(response, "components[0].name")
-      this.skillList = _.uniq(this.skillList.concat(_.map(response, "components[1].name")))
-      console.log(this.skillList);
+
+      this.skillList = _(response)
+                        .map("components[0].name")
+                        .concat(_.map(response, "components[1].name"))
+                        .uniq()
+                        .sortBy()
+                        .value()
     })
+  }
+
+  selectedSkill?: any;
+  availableCombos?: any;
+  primaryCombos?: any;
+  secondaryCombos?: any;
+
+  onSelect(skill: string): void {
+    this.selectedSkill = skill;
+    this.availableCombos = _.filter(this.comboData, (o) => 
+      _.find(o.components, (c) => {
+        return c.name.toLowerCase() === skill.toLowerCase();
+      }));
+
+    this.primaryCombos = _.filter(this.comboData, (o) => 
+      _.find(o.components, (c) => {
+        return c.name.toLowerCase() === skill.toLowerCase() && c.primary;
+      }));
+
+    this.secondaryCombos = this.availableCombos.filter((i: any) => !this.primaryCombos.find((f: any) => f.name === i.name));
+  }
+
+  onDeselect(): void {
+    this.selectedSkill = null;
+    this.availableCombos = null;
   }
 }
